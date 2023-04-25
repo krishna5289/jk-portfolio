639cbf93a802cfa8b2d84cd1a2dc9ad6ed06ba59
krishna-actyv <121858502+krishna-actyv@users.noreply.github.com>
2023-04-25 16:34:01 +0530

    Changed to my details

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -62,7 +62,7 @@ const Header = () => {
             <span>👋</span>
             <div style={{ marginLeft: 20 }}>
               <p className="p-text">Hello, I am</p>
-              <p className="head-text">Praveen</p>
+              <p className="head-text">Krishna</p>
             </div>
           </div>
           <div className="tag-cmp app__flex">
